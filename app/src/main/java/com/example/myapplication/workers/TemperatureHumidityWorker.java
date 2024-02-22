574881b302b65cc1bbd6cadb9a69ce698b5a1a22
Ha <muninnhugin@gmail.com>
2024-02-22 07:20:47 -0800

    add code to check for unavailable sensors

--- app/src/main/java/com/example/myapplication/workers/TemperatureHumidityWorker.java
+++ app/src/main/java/com/example/myapplication/workers/TemperatureHumidityWorker.java
@@ -1,24 +1,42 @@
 package com.example.myapplication.workers;
 
 import android.content.Context;
+import android.hardware.SensorManager;
 
 import androidx.annotation.NonNull;
+import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.example.myapplication.sensors.HumiditySensor;
+import com.example.myapplication.sensors.TemperatureSensor;
+
 public class TemperatureHumidityWorker extends Worker {
-    public TemperatureHumidityWorker(
+
+    private SensorManager sensorManager;
+    private static final String TAG = "TemperatureHumidityWorker";
+
+    public TemperatureHumidityWorker (
             @NonNull Context context,
             @NonNull WorkerParameters params) {
         super(context, params);
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
     }
 
     @Override
     public Result doWork() {
+        TemperatureSensor temperatureSensor = new TemperatureSensor(getApplicationContext());
+        HumiditySensor humiditySensor = new HumiditySensor(getApplicationContext());
+        if(!temperatureSensor.isSensorAvailable() && !humiditySensor.isSensorAvailable())
+        {
+            WorkManager.getInstance(getApplicationContext()).cancelWorkById(this.getId());
+            return Result.failure();
+        }
 
-        // TODO: retrieve temperature and humidity data from sensor
-            // test temperature sensor here
+        // TODO: add notification code here
+        float temperature = temperatureSensor.getTemperature();
 
         return Result.success();
     }
+
 }
