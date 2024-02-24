daa7c240439755631eedd9e55176d208da124a34
Idhant Jena <ijena@uci.edu>
2024-02-23 16:06:43 -0800

    Error with code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-fragment:2.7.6")
-    implementation("androidx.navigation:navigation-ui:2.7.6")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
     implementation("androidx.work:work-runtime:2.9.0")
     implementation("com.google.firebase:firebase-firestore:24.10.2")
     testImplementation("junit:junit:4.13.2")
@@ -53,5 +53,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
+    //for Google Sleep API
+    implementation ("com.google.android.gms:play-services-location:23.1.0")
+
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,14 @@ buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.1")
     }
+    //for Google Sleep API
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
