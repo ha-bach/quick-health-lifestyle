b7df90b27b2c1402f85dbe7b6a9f527796c0bfd9
Ha <muninnhugin@gmail.com>
2024-03-14 12:11:00 -0700

    connect hydration recommendation to home UI

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.example.myapplication.ui.home;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +18,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentHomeBinding;
+import com.example.myapplication.services.HydrationService;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -20,6 +26,9 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
 
 public class HomeFragment extends Fragment {
 
@@ -30,6 +39,8 @@ private FragmentHomeBinding binding;
     FirebaseUser user;
     String userID;
     FirebaseFirestore firestore;
+    private HydrationService hydrationService;
+    private boolean hydrationBound = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
@@ -57,6 +68,10 @@ private FragmentHomeBinding binding;
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        Intent serviceIntent = new Intent(getActivity(), HydrationService.class);
+        getActivity().bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+
         return root;
     }
 
@@ -64,5 +79,38 @@ private FragmentHomeBinding binding;
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        if (hydrationBound) {
+            getActivity().unbindService(serviceConnection);
+            hydrationBound = false;
+        }
     }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            Log.d("MainActivity", "Hydration Service connected.");
+            HydrationService.HydrationBinder hydrationBinder = (HydrationService.HydrationBinder) service;
+            hydrationService = hydrationBinder.getService();
+            hydrationBound = true;
+
+            //        double hydrationRec = 10;
+//        String formattedText = String.format(Locale.getDefault(), "%.2f mL", hydrationRec);
+//        binding.homePlaceholder3.setText(formattedText);
+
+            CompletableFuture<Double> hydrationRecommendation = hydrationService.getHydrationRecommendation();
+            hydrationRecommendation.thenAccept(totalIntake -> {
+                requireActivity().runOnUiThread(() -> {
+                    String formattedText = String.format(Locale.getDefault(), "%.0f mL", totalIntake);
+                    binding.hydrationRecommendationValue.setText(formattedText);
+                });
+            });
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            hydrationBound = false;
+        }
+    };
 }
